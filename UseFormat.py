1b6cf91cfd283b345dfca8090e7f5e6f80fd57d6
Nergiz Özge Erdağı <nergizlatte@outlook.com>
2021-07-10 20:50:18 +0300

    If you want to learn Python language with exercise, look these

---
+++ UseFormat.py
@@ -0,0 +1,5 @@
+egitmen="Nergiz"
+dersAdi= "Python for Basic Level"
+gun="Cumartesi"
+mesaj="Merhaba {} dersiniz {} eğitmeni tarafından {} günü {} saatindedir. Kolay gelsin!".format(dersAdi,egitmen,gun,"20.00")
+print(mesaj)
